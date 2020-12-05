9e73c612b59af2ab8352a67afd2db76a1700d7f9
Francesco Piscani <stem.apks@gmail.com>
2020-12-05 16:30:58 -0500

    Create array_as_parameter.cpp

---
+++ functions/array_as_parameter.cpp
@@ -0,0 +1,16 @@
+ 1 #include <iostream>
+ 2
+ 3 using namespace std;
+ 4
+ 5
+ 6 int main()
+ 7 {
+ 8
+ 9     int A[] = {2,4,6,8,10};
+10     int n = 5;
+11
+12     for(int x:A)
+13         cout << x << " " ;
+14
+15     return 0;
+16 }
