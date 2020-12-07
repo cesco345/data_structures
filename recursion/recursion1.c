041ceb8d194792e870ea28c90283e4077da6cba4
Francesco Piscani <stem.apks@gmail.com>
2020-12-06 21:00:15 -0500

    Create recursion1.c

---
+++ recursion/recursion1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 2
+ 3 void func1(int n)
+ 4 {
+ 5     if(n > 0)
+ 6     {
+ 7         printf("%d \n", n);
+ 8         func1(n-1);
+ 9     }
+10 }
+11
+12 void main()
+13 {
+14     int x = 3;
+15     func1(x);
+16 }
