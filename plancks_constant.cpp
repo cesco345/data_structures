f169e324e24307b1ddc280e063388d91e3e5f68a
Francesco Piscani <stem.apks@gmail.com>
2020-12-12 17:04:46 -0500

    Create plancks_constant.cpp

---
+++ plancks_constant.cpp
@@ -0,0 +1,13 @@
+ 1 #include <cstdio>
+ 2 
+ 3 int main() {
+ 4     double an = 6.0221409e23;
+ 5     double pc = 6.62607004e-34;
+ 6     
+ 7     printf("Avogadro's Number: %lev %lfw %lgx\n", an, an, an);
+ 8     printf("Plancks_constant: %lev %lfw %lgx\n", pc, pc, pc);
+ 9 
+10     float hp = 9.75;
+11     printf("Hogwarts' Platform: %e %f %g\n", hp, hp, hp);
+12 }
+13
