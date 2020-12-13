9407f1736c0923ff3ff91214a1aa5da3dc8539d8
Francesco Piscani <stem.apks@gmail.com>
2020-12-12 21:51:00 -0500

    Create arrays1.cpp

---
+++ arrays/arrays1.cpp
@@ -0,0 +1,9 @@
+ 1 #include <cstdio>
+ 2
+ 3 int main()
+ 4 {
+ 5     int arr[] = { 1, 2, 3, 4 };
+ 6     printf("The third element is %d.\n", arr[2]);
+ 7     arr[2] = 100;
+ 8     printf("The third element is %d.\n", arr[2]);
+ 9 }
